a4b20d7f88b22634fc7ed589527bb46b5b06f714
Alice <carole.2002@outlook.com>
2025-08-29 15:39:40 +0200

    fix(server/auth): fixing sign-in issues

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,7 +1,10 @@
 import { betterAuth } from "better-auth";
 import { username } from "better-auth/plugins/username";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "../../prisma/index";
+import { PrismaClient } from "../../prisma/generated/client/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
