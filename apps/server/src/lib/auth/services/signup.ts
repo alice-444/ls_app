0052b152d732322aa68e1ed19337f347829e1e9f
Alice <carole.2002@outlook.com>
2025-08-29 15:36:25 +0200

    fix (server/auth): sign-up issues

--- apps/server/src/lib/auth/services/signup.ts
+++ apps/server/src/lib/auth/services/signup.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 import { betterAuth } from "better-auth";
 import { username } from "better-auth/plugins/username";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "../../../../prisma/index";
+import { PrismaClient } from "../../../../prisma/generated/client/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 const auth = betterAuth({
 	database: prismaAdapter(prisma, {
