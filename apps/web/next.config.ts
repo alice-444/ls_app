f53ef77d50ccd4816024a37ce73c405d9236eecb
Alice <carole.2002@outlook.com>
2025-10-16 15:47:44 +0200

    refactor(docker): Docker for the development environment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  outputFileTracingRoot: '../../',
+};
 
 export default nextConfig;
