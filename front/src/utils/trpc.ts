fefe7ccfcacad5f607a9fc0d47cd02b87759818c
Alice <carole.2002@outlook.com>
2025-12-02 09:28:31 +0100

    chore: improve project structure across server and web applications for environnement dev

--- front/src/utils/trpc.ts
+++ front/src/utils/trpc.ts
@@ -1,7 +1,7 @@
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "../../../server/src/routers";
+import type { AppRouter } from "../../../back/src/routers";
 import { toast } from "sonner";
 
 export const queryClient = new QueryClient({
